308d3628f37312bda77465abaf7fd2bdf4b9c6f7
Branimir Nachev <branimir.nachev@gmail.com>
2021-09-08 00:43:42 +0300

    Remove Cyrillic

--- main.cpp
+++ main.cpp
@@ -24,66 +24,86 @@ int main() {
 
     auto start = chrono::high_resolution_clock::now();
 
-    Person Petko{"Петко", "Начев"};
-    Person Maria{"Мария", "Начева"};
-    Person Jana{"Снежана", "Христова"};
-    Person Ico{"Христо", "Тодоров"};
-    Person Branimir{"Бранимир", "Начев"};
-    Person Vania{"Ваня", "Христова"};
-    Person Vicky{"Виктория", "Начева"};
+    Person Petko{ "Petko","Nachev" };
+    Person Maria{ "Maria","Nacheva" };
+    Person Jana{ "Snegana","Hristova" };
+    Person Ico{ "Hristo","Todorov" };
+    Person Branimir{ "Branimir","Nachev" };
+    Person Vania{ "Vania","Hristova" };
+    Person Vicky{ "Victoria","Nacheva" };
+    Person Margarita{"Margarita","Nacheva"};
+    Person Stefan{"Stefan","Nikolov"};
+    Person Volen{"Volen","Nikolov"};
 
-    FamilyMember fPetko{Petko};
-    FamilyMember fMaria{Maria};
-    FamilyMember fJana{Jana};
-    FamilyMember fIco{Ico};
-    FamilyMember fBranimir{Branimir, &fMaria, &fPetko};
-    FamilyMember fVania{Vania, &fJana, &fIco};
-    FamilyMember fVicky{Vicky, &fVania, &fBranimir};
+    FamilyMember fPetko{ Petko };
+    FamilyMember fMaria{ Maria };
+    FamilyMember fJana{ Jana };
+    FamilyMember fIco{ Ico };
+    FamilyMember fBranimir{ Branimir,&fMaria,&fPetko };
+    FamilyMember fVania{ Vania,&fJana,&fIco };
+    FamilyMember fVicky{ Vicky,&fVania,&fBranimir };
+    FamilyMember fMargarita {Margarita,&fMaria,&fPetko};
+    FamilyMember fStefan{ Stefan,&fMaria,&fPetko };
+    FamilyMember fVolen (Volen,&fMargarita,&fStefan);
 
     cout << "---------------------------------------------------" << endl;
 
-    cout << "Родители на " << fVicky.getMember().getFullName() <<
-         " са : \n\tМайка => " <<
+    cout << "Parents of " << fVicky.getMember().getFullName() <<
+         " are : \n\tMother => " <<
          fVicky.getMother()->getMember().getFullName() <<
-         "\n и \n\tБаща => " <<
+         "\n and \n\tFather => " <<
          fVicky.getFather()->getMember().getFullName() <<
-         endl << "баба и дядо са : " <<
-         "\n\t Баба => " <<
+         endl << "GrandMa and Pa : " <<
+         "\n\t GrandMother => " <<
          fVicky.getFather()->getMother()->getMember().getFullName() <<
-         "\nи \n\t Дядо => " <<
+         "\nand \n\t GrandFather => " <<
          fVicky.getFather()->getFather()->getMember().getFullName() <<
-         "\n и" <<
-         "\n\t Баба => " <<
+         "\n and" <<
+         "\n\t GrandMother => " <<
          fVicky.getFather()->getMother()->getMember().getFullName() <<
-         "\nи \n\t Дядо => " <<
+         "\nand \n\t GrandFather => " <<
          fVicky.getFather()->getFather()->getMember().getFullName() << endl;
 
     cout << "---------------------------------------------------" << endl;
 
-    cout << fBranimir.getMember().getFullName() << " има дете на име : \n\t" <<
+    cout << fBranimir.getMember().getFullName() << " has a child with name : \n\t" <<
          fBranimir.getChildrens().back()->getMember().getFullName() << endl;
 
-    cout << "Родители на " << fBranimir.getMember().getFullName() <<
-         " са : \n\tМайка => " <<
+    cout << "Parents of " << fBranimir.getMember().getFullName() <<
+         " are : \n\tMother => " <<
          fBranimir.getMother()->getMember().getFullName() <<
-         "\n и \n\tБаща => " <<
+         "\n and \n\tFather => " <<
          fBranimir.getFather()->getMember().getFullName() << endl;
 
     cout << "---------------------------------------------------" << endl;
 
-    cout << fVania.getMember().getFullName() << " има дете на име : \n\t" <<
+    cout << fVania.getMember().getFullName() << " has a child with name : \n\t" <<
          fBranimir.getChildrens().back()->getMember().getFullName() << endl;
 
-    cout << "Родители на " << fVania.getMember().getFullName() <<
-         " са : \n\tМайка => " <<
+    cout << "Parents of " << fVania.getMember().getFullName() <<
+         " are : \n\tMother => " <<
          fVania.getMother()->getMember().getFullName() <<
-         "\n и \n\tБаща => " <<
+         "\n and \n\tFather => " <<
          fVania.getFather()->getMember().getFullName() << endl;
+
+    std::cout << "\nThe cildren of " << fMaria.getMember().getFullName() << " are : " << std::endl;
+
+    for (auto child : fMaria.getChildrens())
+    {
+        std::cout << "\tChild : " << std::endl;
+        std::cout << child->getMember().getFullName() << std::endl;
+        std::cout << "\tHis child : \n" << child->getChildrens().back()->getMember().getFullName() << std::endl;
+        std::cout << "\tAnd her Grand Parents : " << std::endl;
+        std::cout << child->getChildrens().back()->getMother()->getMother()->getMember().getFullName() << std::endl;
+        std::cout << child->getChildrens().back()->getMother()->getFather()->getMember().getFullName() << std::endl;
+        std::cout << child->getChildrens().back()->getFather()->getMother()->getMember().getFullName() << std::endl;
+        std::cout << child->getChildrens().back()->getFather()->getFather()->getMember().getFullName() << std::endl;
+    }
 
     auto end = chrono::high_resolution_clock::now();
 
     chrono::duration<double,std::milli> duration = end - start;
-    cout << "\n\nВреме за итерациите : " << duration.count() << " ms" << endl;
+    cout << "\n\nTime for the iterations : " << duration.count() << " ms" << endl;
 
     return 0;
 }
